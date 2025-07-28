8cdabb57fc6ee712e97aeeaf018a82dd7708b8c4
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-07-28 16:43:34 -0300

    adding validation

--- Program.cs
+++ Program.cs
@@ -43,9 +43,38 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
 #endregion
 
 #region Veiculos
+
+ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
+{
+    var validacao = new ErrosDeValidacao
+    {
+        Mensagens = new List<string>()
+    };
+
+    if (string.IsNullOrEmpty(veiculoDTO.Nome))
+    {
+        validacao.Mensagens.Add("O nome não pode ser vazio");
+    }
+    if (string.IsNullOrEmpty(veiculoDTO.Marca))
+    {
+        validacao.Mensagens.Add("A marca não pode ficar em branco");
+    }
+    if (veiculoDTO.Ano < 1950)
+    {
+        validacao.Mensagens.Add("Veiculo muito antigo, aceito somento anos superiores a 1950");
+    }
+
+    return validacao;
+}
 app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
 {
+    
 
+    var validacao = validaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+    {
+        return Results.BadRequest(validacao);
+    }
     var veiculo = new Veiculo
     {
         Nome = veiculoDTO.Nome,
@@ -80,8 +109,13 @@ app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeicul
 {
 
     var veiculo = veiculoServico.BuscaPorId(id);
-
     if (veiculo == null) return Results.NotFound();
+    
+    var validacao = validaDTO(veiculoDTO);
+    if (validacao.Mensagens.Count > 0)
+    {
+        return Results.BadRequest(validacao);
+    }
 
     veiculo.Nome = veiculoDTO.Nome;
     veiculo.Marca = veiculoDTO.Marca;
